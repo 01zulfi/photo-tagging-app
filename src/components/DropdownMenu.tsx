c343ba4913a9802578763840763aff5b5cab8a2f
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-23 20:07:02 +0500

    Add DropdownMenu

---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,45 @@
+import React, { FC } from 'react';
+
+interface Props {
+  clickPosition: number[];
+}
+
+const DropdownMenu: FC<Props> = ({ clickPosition }) => {
+  const [x, y] = clickPosition;
+
+  return (
+    <div
+      style={{
+        width: 'fit-content',
+        position: 'absolute',
+        left: x,
+        top: y,
+      }}
+    >
+      <ul
+        style={{
+          position: 'absolute',
+          top: '0',
+          zIndex: '1',
+          width: 'fit-content',
+          padding: '10px',
+          listStyle: 'none',
+          border: '1px solid rgba(255, 255, 255, 0.1)',
+          borderRadius: '10px',
+          boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.1)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '10px',
+          backgroundColor: 'white',
+        }}
+      >
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- src/components/PhotoContainer.tsx
+++ src/components/PhotoContainer.tsx
@@ -1,24 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import photo from '../images/egor-klyuchnyk-full-x-season-web.jpg';
 import calculateCoordinates from './utils/calculate-coordinates';
+import DropdownMenu from './DropdownMenu';
 
 const PhotoContainer: FC = () => {
-  const style = { width: '90%' };
+  const [clickPosition, setClickPosition] = useState([0, 0]);
+  const style = { width: '90%', cursor: 'crosshair' };
 
   const handleClick = (event: any) => {
-    const { x, y } = calculateCoordinates(event);
-    console.log({ x, y });
+    calculateCoordinates(event);
+    setClickPosition([event.pageX, event.pageY]);
   };
 
   return (
-    <div className="photo-container">
-      <img
-        onClick={handleClick}
-        aria-hidden="true"
-        src={photo}
-        alt="find-characters"
-        style={style}
-      />
+    <div>
+      <div className="photo-container">
+        <img
+          onClick={handleClick}
+          aria-hidden="true"
+          src={photo}
+          alt="find-characters"
+          style={style}
+        />
+      </div>
+      <DropdownMenu clickPosition={clickPosition} />
     </div>
   );
 };
