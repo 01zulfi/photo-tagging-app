ad9c3129c3c59dfeb6a1f942e8cf9d1ef660c3a0
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-26 16:03:25 +0500

    Add ImageContainer.test.tsx

---
+++ src/tests/ImageContainer.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ImageContainer from '../components/ImageContainer';
+
+test('image renders', () => {
+  const { getByAltText } = render(
+    <ImageContainer handleImageClick={() => {}} />,
+  );
+  const image = getByAltText('find-characters');
+
+  expect(image).not.toBeNull();
+  expect(image).toBeInTheDocument();
+});
+
+test('calls handleImageClick upon image click', () => {
+  const handleImageClick = jest.fn();
+  const { getByAltText } = render(
+    <ImageContainer handleImageClick={handleImageClick} />,
+  );
+  const image = getByAltText('find-characters');
+
+  fireEvent.click(image);
+
+  expect(handleImageClick).toHaveBeenCalled();
+});
