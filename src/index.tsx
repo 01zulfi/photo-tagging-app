91d979d46b76b7ad073e87135a959c65855f3a88
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-27 19:40:09 +0500

    Initialize firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { initializeApp } from 'firebase/app';
 import App from './App';
+import firebaseConfig from './firebase/firebase-config';
+
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
