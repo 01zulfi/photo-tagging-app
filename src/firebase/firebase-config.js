e53a992b989780415492391a2d605b08d63c5d7f
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-27 19:10:16 +0500

    Add firebase config object

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKY4ijgh2aQqjyq2sdFaiIpBJ8Hz0U7jg',
+  authDomain: 'find-the-characters.firebaseapp.com',
+  projectId: 'find-the-characters',
+  storageBucket: 'find-the-characters.appspot.com',
+  messagingSenderId: '75741157329',
+  appId: '1:75741157329:web:ec1e991dfd3bb476b3f2f0',
+};
+
+export default firebaseConfig;
