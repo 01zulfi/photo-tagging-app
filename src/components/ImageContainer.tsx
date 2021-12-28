a8770c12c105dc2eafc43c9d71d58aff0e3dff83
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-28 18:00:54 +0500

    Add functionality to get image from firebase storage

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -1,23 +1,38 @@
-import React, { FC } from 'react';
-import photo from '../images/egor-klyuchnyk-full-x-season-web.jpg';
+import React, { FC, useState, useEffect } from 'react';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 interface ImageContainerProps {
   handleImageClick: any;
 }
 
 const ImageContainer: FC<ImageContainerProps> = ({ handleImageClick }) => {
+  const [imageURL, setImageURL] = useState('');
   const style = { width: '90%', cursor: 'crosshair' };
+
+  useEffect(() => {
+    (async () => {
+      const storage = getStorage();
+      const fetchURL = await getDownloadURL(
+        ref(storage, 'egor-klyuchnyk-full-x-season-web.jpg'),
+      );
+      setImageURL(fetchURL);
+    })();
+  }, []);
 
   return (
     <div>
       <div className="image-container">
-        <img
-          onClick={handleImageClick}
-          aria-hidden="true"
-          src={photo}
-          alt="find-characters"
-          style={style}
-        />
+        {imageURL ? (
+          <img
+            onClick={handleImageClick}
+            aria-hidden="true"
+            src={imageURL}
+            alt="find-characters"
+            style={style}
+          />
+        ) : (
+          <h2>Loading...</h2>
+        )}
       </div>
     </div>
   );
