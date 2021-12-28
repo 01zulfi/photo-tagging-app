91a1f215bf925970a016b2c0b9976990f88a3dda
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-28 20:47:10 +0500

    Get characters data & check against coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { FC, useEffect, useState } from 'react';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import Dropdown from './components/Dropdown';
 import ImageContainer from './components/ImageContainer';
 import Start from './components/Start';
 import Navbar from './components/Navbar';
 import calculateCoordinates from './utils/calculate-coordinates';
 import characters from './utils/characters';
+import areCoordinatesInRange from './utils/are-coordinates-in-range';
+import Status from './components/Status';
 
 interface Coordinates {
   x: number;
@@ -22,7 +25,11 @@ const App: FC = () => {
     x: 0,
     y: 0,
   });
-  const [currentCharacter, setCurrentCharacter] = useState<any>(null);
+  const [currentCharacter, setCurrentCharacter] = useState<{
+    name: string;
+    id: string;
+  }>({ name: ' ', id: '' });
+  const [success, setSuccess] = useState(false);
 
   const handleStartClick = (): void => setIsStartClicked(true);
 
@@ -34,16 +41,36 @@ const App: FC = () => {
     setDropdownPosition({ x: event.pageX, y: event.pageY });
   };
 
+  const getCharacterData = async (character: string) => {
+    const characterRef = doc(getFirestore(), 'characters', character);
+    const characterSnap = await getDoc(characterRef);
+    return characterSnap.data();
+  };
+
   const handleDropdownClick = (
     event: React.MouseEvent<HTMLLIElement>,
   ): void => {
     setShouldDropdownRender(false);
     const id = event.currentTarget.getAttribute('data-id');
     const targetCharacter = characters.find((character) => character.id === id);
-    setCurrentCharacter(targetCharacter);
+    if (targetCharacter) setCurrentCharacter(targetCharacter);
   };
 
   useEffect(() => {
+    (async () => {
+      const data = await getCharacterData(currentCharacter.name);
+      if (data) {
+        const { xMin, xMax, yMin, yMax } = data;
+        const flag = areCoordinatesInRange(clickCoordinates, {
+          xMin,
+          xMax,
+          yMin,
+          yMax,
+        });
+        setSuccess(flag);
+        console.log(flag);
+      }
+    })();
     console.log({ currentCharacter, clickCoordinates });
   }, [currentCharacter]);
 
@@ -62,6 +89,7 @@ const App: FC = () => {
           handleDropdownClick={handleDropdownClick}
         />
       )}
+      {success && <Status success={success} />}
     </div>
   );
 };
