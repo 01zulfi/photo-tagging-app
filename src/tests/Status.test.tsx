c0c344f29af59f497ccb3e3bbc667d4219b08000
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-30 17:48:08 +0500

    Add test for render handler prop

--- src/tests/Status.test.tsx
+++ src/tests/Status.test.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Status from '../components/Status';
 
+jest.useFakeTimers();
+
 test('renders correct text when success true', () => {
-  const { getByText } = render(<Status success />);
+  const { getByText } = render(<Status renderHandler={() => {}} success />);
   expect(getByText(/great! you found the character!/i)).toBeInTheDocument();
 });
 
 test('renders correct text when success false', () => {
-  const { getByText } = render(<Status success={false} />);
+  const { getByText } = render(
+    <Status renderHandler={() => {}} success={false} />,
+  );
   expect(getByText(/that's not it! keep looking!/i)).toBeInTheDocument();
+});
+
+test('calls render handler with false after 1500 ms', () => {
+  const renderHandler = jest.fn();
+  act(() => {
+    render(<Status success renderHandler={renderHandler} />);
+  });
+
+  expect(renderHandler).not.toHaveBeenCalled();
+
+  jest.runAllTimers();
+
+  expect(renderHandler).toHaveBeenCalledTimes(1);
+  expect(renderHandler).toHaveBeenCalledWith(false);
 });
