f7f01a8ca7d4d9b9fd485e14a9429fd6bab9f509
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-31 18:34:10 +0500

    Add Timer.tsx

---
+++ src/components/Timer.tsx
@@ -0,0 +1,23 @@
+import React, { FC, useState, useEffect } from 'react';
+
+interface TimerProps {
+  shouldTimerStart: boolean;
+}
+
+const Timer: FC<TimerProps> = ({ shouldTimerStart }) => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    if (!shouldTimerStart) return () => {};
+    console.log(seconds);
+    const time = setInterval(
+      () => setSeconds((prevProps) => prevProps + 1),
+      1000,
+    );
+    return () => clearInterval(time);
+  });
+
+  return <p>{seconds}</p>;
+};
+
+export default Timer;
