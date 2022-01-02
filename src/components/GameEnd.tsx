34593910e649becf704c466a938b36f403c6ee20
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-02 19:42:42 +0500

    Add inputHandler & submitHandler (TODO: hook up with firebase)

--- src/components/GameEnd.tsx
+++ src/components/GameEnd.tsx
@@ -1,12 +1,20 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
 
 const GameEnd: FC = () => {
+  const [name, setName] = useState('');
+
   useEffect(() => {
     const auth = getAuth();
     signInAnonymously(auth).catch((error) => console.log(error));
     onAuthStateChanged(auth, (user) => console.log(user));
   }, []);
+
+  const inputHandler = (event: any) => setName(event.target.value);
+
+  const submitHandler = () => {
+    console.log(name);
+  };
 
   return (
     <section
@@ -20,6 +28,14 @@ const GameEnd: FC = () => {
       }}
     >
       <h2>Congratulations! You found all of the characters!</h2>
+      <h3>Submit your score</h3>
+      <label htmlFor="name-input">
+        Enter name:
+        <input onChange={inputHandler} type="text" id="name-input" />
+      </label>
+      <button onClick={submitHandler} type="button">
+        Submit Score
+      </button>
     </section>
   );
 };
