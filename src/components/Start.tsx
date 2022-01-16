0e6868e6a0fa67459bba44de5b2d77465c9e2101
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-16 17:12:06 +0500

    Add images to Start.tsx

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,6 +1,9 @@
 import React, { FC, useEffect, useState } from 'react';
 import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import Loading from './Loading';
+import tom from '../images/tom.png';
+import batman from '../images/batman.png';
+import neo from '../images/neo-matrix.png';
 
 interface StartProps {
   handleStartClick: () => void;
@@ -15,8 +18,28 @@ const Start: FC<StartProps> = ({ handleStartClick }) => {
     });
   }, []);
 
+  const imageStyles = {
+    width: '50px',
+    aspectRatio: '1',
+  };
+
   return (
     <section>
+      <div>
+        <h2>Characters to find:</h2>
+        <div>
+          <img src={tom} alt="tom" style={imageStyles} />
+          <p>Tom from Tom and Jerry</p>
+        </div>
+        <div>
+          <img src={neo} alt="neo" style={imageStyles} />
+          <p>Neo from The Matrix</p>
+        </div>
+        <div>
+          <img src={batman} alt="batman" style={imageStyles} />
+          <p>Batman</p>
+        </div>
+      </div>
       {canStart ? (
         <button type="button" id="start" onClick={handleStartClick}>
           <h2>Start</h2>
@@ -24,14 +47,6 @@ const Start: FC<StartProps> = ({ handleStartClick }) => {
       ) : (
         <Loading />
       )}
-      <div>
-        Characters to find:
-        <ul>
-          <li>Tom</li>
-          <li>Neo</li>
-          <li>Batman</li>
-        </ul>
-      </div>
     </section>
   );
 };
