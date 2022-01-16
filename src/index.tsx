ce3354077b4a67e9b0dd87092a0797e4cb2d56f9
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-16 17:56:37 +0500

    Add reset.css & normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { initializeApp } from 'firebase/app';
 import App from './App';
 import firebaseConfig from './firebase/firebase-config';
+import './styles/reset.css';
+import './styles/normalize.css';
 
 initializeApp(firebaseConfig);
 
