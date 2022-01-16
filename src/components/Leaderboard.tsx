4a4d630281412611552cfd89c1ebdc16404db58b
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-16 16:00:29 +0500

    Display sorted scores in Leaderboard.tsx

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,5 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import firebase from '../firebase/firebase';
 
-const Leaderboard: FC = () => <h1>Leaderboard</h1>;
+const Leaderboard: FC = () => {
+  const [list, setList] = useState<any>([]);
+
+  useEffect(() => {
+    (async () => {
+      const scores = await firebase.fetchScores();
+      const filteredScores = scores.sort((a, b) => a.score - b.score);
+      setList(filteredScores);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      {list.map((item: any) => (
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.score}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Leaderboard;
