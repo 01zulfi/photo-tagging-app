197db10047fc92fe00845ea3f1ab37113998ece0
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-16 18:45:25 +0500

    Style Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,50 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 import Timer from './Timer';
 
 interface NavbarProps {
   shouldTimerStart: boolean;
 }
 
+const StyledNavbar = styled.header`
+  @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #384058;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+const StyledHeading = styled.h1`
+  color: #d3deff;
+  font-weight: bolder;
+`;
+
+const StyledLBButton = styled.button`
+  height: fit-content;
+  padding: 20px;
+  background-color: #ffb84d;
+  border: 0;
+  border-radius: 10px;
+`;
+
+const Zero = styled.p`
+  font-family: 'Anton', 'sans-serif';
+  color: white;
+  font-size: 2rem;
+  background: black;
+  width: 25%;
+  text-align: center;
+`;
+
 const Navbar: FC<NavbarProps> = ({ shouldTimerStart }) => (
-  <div className="navbar">
-    <h1>Photo Tagging App</h1>
-    {shouldTimerStart && <Timer />}
-    <button className="leaderboard" type="button">
-      Leaderboard
-    </button>
-  </div>
+  <StyledNavbar>
+    <StyledHeading>find the characters</StyledHeading>
+    {shouldTimerStart ? <Timer /> : <Zero>0</Zero>}
+    <StyledLBButton>Leaderboard</StyledLBButton>
+  </StyledNavbar>
 );
 export default Navbar;
