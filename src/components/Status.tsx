d0dbc71ebef07a5b95810906271926f73e5b841c
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-18 15:23:52 +0500

    Style Status.tsx

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,4 +1,15 @@
 import React, { FC, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 15%;
+  background: white;
+  padding: 20px;
+  background: ${(props: any) => (props.success ? '#84FFFF' : '#EF9A9A')};
+  border-radius: 10px;
+  font-size: 1.1em;
+`;
 
 interface StatusProps {
   success: boolean;
@@ -11,13 +22,13 @@ const Status: FC<StatusProps> = ({ renderHandler, success }) => {
   }, []);
 
   return (
-    <div style={{ position: 'fixed', top: '15%', background: 'white' }}>
+    <Wrapper success={success}>
       {success ? (
         <p>Great! You found the character!</p>
       ) : (
         <p>That&apos;s not it! Keep looking!</p>
       )}
-    </div>
+    </Wrapper>
   );
 };
 
