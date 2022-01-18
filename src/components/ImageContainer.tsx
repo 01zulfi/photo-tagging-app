954d62a7c678b787bb104da80025adc759da85b5
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-18 13:06:10 +0500

    Refactor get image url logic; style ImageContainer.tsx

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -1,6 +1,18 @@
-import React, { FC, useState, useEffect, useRef } from 'react';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-import Loading from './Loading';
+import React, { FC, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import firebase from '../firebase/firebase';
+
+const Container = styled.div`
+  display: ${(props: any) => (props.opacity ? 'block' : 'none')};
+  pointer-events: ${(props: any) => (props.opacity ? 'auto' : 'none')};
+  width: fit-content;
+`;
+
+const Image = styled.img`
+  height: ${(props: any) => (props.opacity ? 'fit-content' : ' 0px')};
+  width: 100%;
+  cursor: crosshair;
+`;
 
 interface ImageContainerProps {
   handleImageClick: any;
@@ -12,49 +24,26 @@ const ImageContainer: FC<ImageContainerProps> = ({
   imageOpacity,
 }) => {
   const [imageURL, setImageURL] = useState('');
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
-  const image = useRef<any>(null);
 
   useEffect(() => {
     (async () => {
-      const storage = getStorage();
-      const fetchURL = await getDownloadURL(
-        ref(storage, 'egor-klyuchnyk-full-x-season-web.jpg'),
-      );
-      setImageURL(fetchURL);
+      const URL = await firebase.fetchImageURL();
+      setImageURL(URL);
     })();
   }, []);
 
-  useEffect(() => {
-    if (!imageURL) return;
-    const onLoadCallback = () => {
-      setIsImageLoaded(true);
-    };
-    image.current.onload = onLoadCallback;
-  }, [imageURL]);
-
   return (
-    <div
-      style={{
-        opacity: imageOpacity,
-        pointerEvents: imageOpacity ? 'auto' : 'none',
-        height: imageOpacity ? 'fit-content' : '0px',
-      }}
-    >
-      <div className="image-container">
-        {!isImageLoaded && <Loading />}
-        {imageURL && (
-          <img
-            onClick={handleImageClick}
-            aria-hidden="true"
-            src={imageURL}
-            alt="find-characters"
-            style={{ width: '90%', cursor: 'crosshair' }}
-            ref={image}
-          />
-        )}
-      </div>
-    </div>
+    <Container opacity={imageOpacity}>
+      {imageURL && (
+        <Image
+          onClick={handleImageClick}
+          aria-hidden="true"
+          src={imageURL}
+          alt="find-characters"
+          opacity={imageOpacity}
+        />
+      )}
+    </Container>
   );
 };
 
