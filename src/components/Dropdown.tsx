6a2bdfe1555f6a1c318464b95f4f8a64661cf888
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-18 15:10:46 +0500

    Style dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,41 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: fit-content;
+  position: absolute;
+  left: ${(props: any) => `${props.x}px`};
+  top: ${(props: any) => `${props.y}px`};
+`;
+
+const List = styled.ul`
+  position: absolute;
+  top: 0;
+  z-index: 1;
+  width: fit-content;
+  padding: 10px;
+  list-style: none;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 10px;
+  box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  alignitems: center;
+  gap: 10px;
+  color: white;
+  background-color: rgba(18, 18, 18, 0.9);
+`;
+
+const ListItem = styled.li`
+  font-weight: 800;
+  padding: 10px;
+  text-align: center;
+  border-radius: 5px;
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(255, 255, 255, 0.11);
+  }
+`;
 
 interface DropdownProps {
   dropdownPosition: { x: number; y: number };
@@ -14,44 +51,20 @@ const Dropdown: FC<DropdownProps> = ({
   const { x, y } = dropdownPosition;
 
   return (
-    <div
-      style={{
-        width: 'fit-content',
-        position: 'absolute',
-        left: x,
-        top: y,
-      }}
-    >
-      <ul
-        style={{
-          position: 'absolute',
-          top: '0',
-          zIndex: '1',
-          width: 'fit-content',
-          padding: '10px',
-          listStyle: 'none',
-          border: '1px solid rgba(255, 255, 255, 0.1)',
-          borderRadius: '10px',
-          boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.1)',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: '10px',
-          backgroundColor: 'white',
-        }}
-      >
+    <Wrapper x={x} y={y}>
+      <List>
         {characters.map((character) => (
-          <li
+          <ListItem
             key={character.id}
             aria-hidden="true"
             onClick={handleDropdownClick}
             data-id={character.id}
           >
             {character.name}
-          </li>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </Wrapper>
   );
 };
 
