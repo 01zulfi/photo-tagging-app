f37e6e0f2fc536dacd64b12a7bfc38e16d716df2
Zulfiqar Ali <01zulfiqarali@gmail.com>
2022-01-21 11:16:26 +0500

    Update Navbar.test.tsx

--- src/tests/Navbar.test.tsx
+++ src/tests/Navbar.test.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 test('matches snapshot', () => {
-  expect(render(<Navbar shouldTimerStart={false} />)).toMatchSnapshot();
+  expect(
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Navbar shouldTimerStart={false} />} />
+        </Routes>
+      </BrowserRouter>,
+    ),
+  ).toMatchSnapshot();
 });
 
 test('renders correct heading', () => {
-  const { getByRole } = render(<Navbar shouldTimerStart={false} />);
+  const { getByRole } = render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Navbar shouldTimerStart={false} />} />
+      </Routes>
+    </BrowserRouter>,
+  );
   expect(getByRole('heading')).toHaveTextContent(/find the characters/i);
 });
